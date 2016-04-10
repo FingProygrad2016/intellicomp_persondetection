dce2d0fe7fa6e0e9ab364caaa6fb77002aa0271d
isilveira <ismaelsilca@gmail.com>
2016-04-10 04:58:05 -0300

    Se agrega la opción de expandir los blobs

--- trackermaster/black_boxes/blob_detection.py
+++ trackermaster/black_boxes/blob_detection.py
@@ -9,7 +9,7 @@ from imutils.object_detection import non_max_suppression
 # http://www.learnopencv.com/blob-detection-using-opencv-python-c/
 
 
-def find_blobs_bounding_boxes(bg_image):
+def find_blobs_bounding_boxes(bg_image, expand_blobs):
         """
         Find bounding boxes for each element of 'blobs'
         :param bg_image: the image containing the blobs
@@ -20,7 +20,13 @@ def find_blobs_bounding_boxes(bg_image):
                                                     cv2.CHAIN_APPROX_SIMPLE)
         bounding_boxes = []
         for contour in contours:
-            bounding_boxes.append(cv2.boundingRect(contour))
+            x, y, w, h = cv2.boundingRect(contour)
+            if expand_blobs[0]:
+                x = max(x - ((w * expand_blobs[1]) / 2), 0)
+                y = max(y - ((h * expand_blobs[1]) / 2), 0)
+                w = min(w * (expand_blobs[1] + 1), bg_image.shape[1])
+                h = min(h * (expand_blobs[1] + 1), bg_image.shape[0])
+            bounding_boxes.append((x, y, w, h))
         return bounding_boxes
 
 
@@ -58,6 +64,9 @@ class BlobDetector:
             config.getint('SMALL_BLOBS_SIZE_DISTANCE_THRESHOLD')
         self.detect_blobs_by_bounding_boxes = \
             config.getboolean('DETECT_BLOBS_BY_BOUNDING_BOXES')
+        self.expand_blobs = \
+            (config.getboolean('EXPAND_BLOBS'),
+             config.getfloat('EXPAND_BLOBS_RATIO'))
 
         # Setup SimpleBlobDetector parameters
         params = cv2.SimpleBlobDetector_Params()
@@ -101,57 +110,77 @@ class BlobDetector:
         self.min_person_blob_size = 0
         self.max_person_blob_size = 1000000
 
-    # Try to identify small blobs with big blobs
-    def identify_small_blobs(self):
-        result = []
-        small_blob_to_exclude = []
-
-        for smallBlobIndex, smallBlob in enumerate(self.small_blobs):
-            candidate = (None, 100000)
-            for bigBlobIndex, bigBlob in enumerate(self.big_blobs):
-                dist = euclidean_distance(smallBlob.pt, bigBlob.pt)
-                if (dist < self.small_blobs_size_distance_threshold) and \
-                        (dist < candidate[1]):
-                    candidate = (bigBlobIndex, dist - (bigBlob.size / 2))
-            if candidate[0]:
-                # Calculo tamano total de la adicion del small blob
-                # Uso max por el caso en que las areas se intersecten
-                self.big_blobs[candidate[0]].size = \
-                    max(self.big_blobs[candidate[0]].size,
-                        self.big_blobs[candidate[0]].size + candidate[1])
-
-                # Calculo punto medio ponderando por tamano de los blobs
-                total_size = smallBlob.size + self.big_blobs[candidate[0]].size
-                x_new = (self.big_blobs[candidate[0]].pt[0] *
-                         self.big_blobs[candidate[0]].size +
-                         smallBlob.pt[0] * smallBlob.size) / total_size
-                y_new = (self.big_blobs[candidate[0]].pt[1] *
-                         self.big_blobs[candidate[0]].size +
-                         smallBlob.pt[1] * smallBlob.size) / total_size
-                self.big_blobs[candidate[0]].pt = (x_new, y_new)
-                small_blob_to_exclude.append(smallBlob)
-
-        for blob in small_blob_to_exclude:
-            self.small_blobs.remove(blob)
-
-        # for smallBlob in self.small_blobs:
-        #     result.append(smallBlob)
-
-        for bigBlob in self.big_blobs:
-            result.append(bigBlob)
-
-        return result
+    # # Try to identify small blobs with big blobs
+    # def identify_small_blobs(self):
+    #     result = []
+    #     small_blob_to_exclude = []
+    #
+    #     for smallBlobIndex, smallBlob in enumerate(self.small_blobs):
+    #         candidate = (None, 100000)
+    #         for bigBlobIndex, bigBlob in enumerate(self.big_blobs):
+    #             dist = euclidean_distance(smallBlob.pt, bigBlob.pt)
+    #             if (dist < self.small_blobs_size_distance_threshold) and \
+    #                     (dist < candidate[1]):
+    #                 candidate = (bigBlobIndex, dist - (bigBlob.size / 2))
+    #         if candidate[0]:
+    #             # Calculo tamano total de la adicion del small blob
+    #             # Uso max por el caso en que las areas se intersecten
+    #             self.big_blobs[candidate[0]].size = \
+    #                 max(self.big_blobs[candidate[0]].size,
+    #                     self.big_blobs[candidate[0]].size + candidate[1])
+    #
+    #             # Calculo punto medio ponderando por tamano de los blobs
+    #             total_size = smallBlob.size + self.big_blobs[candidate[0]].size
+    #             x_new = (self.big_blobs[candidate[0]].pt[0] *
+    #                      self.big_blobs[candidate[0]].size +
+    #                      smallBlob.pt[0] * smallBlob.size) / total_size
+    #             y_new = (self.big_blobs[candidate[0]].pt[1] *
+    #                      self.big_blobs[candidate[0]].size +
+    #                      smallBlob.pt[1] * smallBlob.size) / total_size
+    #             self.big_blobs[candidate[0]].pt = (x_new, y_new)
+    #             small_blob_to_exclude.append(smallBlob)
+    #
+    #     for blob in small_blob_to_exclude:
+    #         self.small_blobs.remove(blob)
+    #
+    #     # for smallBlob in self.small_blobs:
+    #     #     result.append(smallBlob)
+    #
+    #     for bigBlob in self.big_blobs:
+    #         result.append(bigBlob)
+    #
+    #     return result
 
     def apply(self, background):
         blobs = []
         if self.detect_blobs_by_bounding_boxes:
-            blobs = find_blobs_bounding_boxes(background)
+            blobs = find_blobs_bounding_boxes(background, self.expand_blobs)
         else:
             for keyPoint in self.detector.detect(background):
-                x1 = int(max(keyPoint.pt[0] - keyPoint.size, 0))
-                y1 = int(max(keyPoint.pt[1] - keyPoint.size, 0))
-                x2 = int(min(keyPoint.pt[0] + keyPoint.size, background.shape[1]))
-                y2 = int(min(keyPoint.pt[1] + keyPoint.size, background.shape[0]))
+                if self.expand_blobs[0]:
+                    x1 = max(keyPoint.pt[0] - keyPoint.size, 0)
+                    y1 = max(keyPoint.pt[1] - keyPoint.size, 0)
+                    x2 = min(keyPoint.pt[0] + keyPoint.size,
+                             background.shape[1])
+                    y2 = min(keyPoint.pt[1] + keyPoint.size,
+                             background.shape[0])
+
+                    w = x2 - x1
+                    h = y2 - y1
+
+                    x1 = max(x1 - ((w * self.expand_blobs[1]) / 2), 0)
+                    y1 = max(y1 - ((h * self.expand_blobs[1]) / 2), 0)
+                    x2 = min(x2 + ((w * self.expand_blobs[1]) / 2),
+                             background.shape[1])
+                    y2 = min(y2 + ((h * self.expand_blobs[1]) / 2),
+                             background.shape[0])
+                else:
+                    x1 = max(keyPoint.pt[0] - keyPoint.size, 0)
+                    y1 = max(keyPoint.pt[1] - keyPoint.size, 0)
+                    x2 = min(keyPoint.pt[0] + keyPoint.size,
+                             background.shape[1])
+                    y2 = min(keyPoint.pt[1] + keyPoint.size,
+                             background.shape[0])
                 blobs.append((x1, y1, x2 - x1, y2 - y1))
         if blobs:
             blobs = non_max_suppression(x1y1wh_to_x1y1x2y2(blobs),
